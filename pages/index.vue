f4ccaff0350d688e4577157e3031596e09627691
Regan Ya <hens.virtuella.varld@gmail.com>
2024-05-30 22:48:31 +0200

    feat: add some pages

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+      necessitatibus temporibus officiis tenetur tempora consectetur dolor eius
+      reiciendis ad, illo porro veniam inventore quasi enim fugit! Unde officia
+      officiis eligendi!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+      necessitatibus temporibus officiis tenetur tempora consectetur dolor eius
+      reiciendis ad, illo porro veniam inventore quasi enim fugit! Unde officia
+      officiis eligendi!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>I am product</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
