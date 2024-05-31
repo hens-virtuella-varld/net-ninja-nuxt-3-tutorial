7d329472a449fd0eb73030eaa9ace4a9490ddb44
Regan Ya <hens.virtuella.varld@gmail.com>
2024-05-31 23:11:26 +0200

    feat: add NuxtLink to practice it

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Net Ninja Nuxt 3 Tutorial</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+         <a href="/about">normal link -- about page</a>
+      </nav>
+    </header>
     <h2>About</h2>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
@@ -19,11 +30,11 @@
 <script setup></script>
 
 <style scoped>
-  h2 {
-    margin-bottom: 20px;
-    font-size: 36px;
-  }
-  p {
-    margin: 20px 0;
-  }
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
 </style>
