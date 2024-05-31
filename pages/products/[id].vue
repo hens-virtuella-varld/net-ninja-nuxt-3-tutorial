63f0d36e95dbec257de8eb86002cde46806a4bd2
Regan Ya <hens.virtuella.varld@gmail.com>
2024-05-31 22:28:39 +0200

    feat: add [id] component to practice route parameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum temporibus
+      nesciunt aspernatur inventore corrupti, possimus numquam mollitia eveniet.
+      Pariatur tempora numquam explicabo atque soluta quae facilis harum
+      expedita tempore deserunt.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
