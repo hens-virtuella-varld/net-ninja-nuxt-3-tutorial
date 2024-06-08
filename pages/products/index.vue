c65b985887c3abd3ecd302be60ab516aecc94cdc
Regan Ya <hens.virtuella.varld@gmail.com>
2024-06-08 22:32:32 +0200

    feat: add customised head metadata in product index page

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -9,9 +9,14 @@
 </template>
 
 <script setup>
-const { data: products } = await useFetch('https://fakestoreapi.com/products');
-
 definePageMeta({
   layout: 'products',
+});
+
+const { data: products } = await useFetch('https://fakestoreapi.com/products');
+
+useHead({
+  title: 'Net Ninja | Merch',
+  meta: [{ name: 'description', content: 'Nuxt 3 Merch' }],
 });
 </script>
