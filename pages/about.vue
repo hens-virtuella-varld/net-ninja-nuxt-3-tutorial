5713352b4cac0ed8ce11b19b3a652ad5c539062f
Regan Ya <hens.virtuella.varld@gmail.com>
2024-06-09 00:10:04 +0200

    feat: add currency api and use it

--- pages/about.vue
+++ pages/about.vue
@@ -18,10 +18,8 @@
 </template>
 
 <script setup>
-const { data } = await useFetch('/api/ninja?name=mario', {
-  method: 'post',
-  body: { age: 30 },
-});
+// const { data } = await useFetch('/api/ninja');
+const { data } = await useFetch('/api/currency/GBP');
 </script>
 
 <style scoped>
--- server/api/ninja.js
+++ server/api/ninja.js
@@ -1,11 +1,16 @@
 export default defineEventHandler(async (event) => {
+  const { currencyKey } = useRuntimeConfig();
+
   // handle query params
-  const { name } = getQuery(event);
+  // const { name } = getQuery(event);
 
   // handle post data
-  const { age } = await readBody(event);
+  // const { age } = await readBody(event);
 
-  return {
-    message: `Hello, ${name}! You are ${age} years old.`,
-  };
+  // api call with private key
+  const { data } = await $fetch(
+    `https://api.currencyapi.com/v3/latest?apikey=${currencyKey}`
+  );
+
+  return data;
 });
